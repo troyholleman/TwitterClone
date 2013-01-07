34e24b31df0bb4f66dc6137bc9acf9546a2ffed8
Troy <troyholleman@gmail.com>
2013-01-07 14:53:36 -0500

    Initial Rails setup + Devise for auth.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterTest::Application.config.secret_token = '774d61a0881b483540cd2084a23dfa731973925bbd6bcab2c8b83fd341c3bb97a740df0b79b8ae8222a26bfefcf7376fac83d54900ac8aa0f3a182789ea6bcd0'
